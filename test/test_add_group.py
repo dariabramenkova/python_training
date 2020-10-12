473b447bb5bb7755581a6d28dcb3273d9cb8bcc6
dariabramenkova <dvchca_12@mail.ru>
2020-10-13 01:23:29 +0300

    Добавлена проверка текущего состояния сессии

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,5 +7,6 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="gruppa", header="druppa", footer="druppa2"))
 
+
 def test_add_group_empty(app):
     app.group.create(Group(name="", header="", footer=""))
