a58652b928035d3b620c54e366984e19aab26686
dariabramenkova <dvchca_12@mail.ru>
2020-10-16 01:50:58 +0300

    Информация на исх. странице и на странице редактирования

--- fixture/client.py
+++ fixture/client.py
@@ -113,5 +113,37 @@ class ClientHelper:
                 firstname = cells[2].text
                 secoundname = cells[1].text
                 id = element.find_element_by_name("selected[]").get_attribute("value")
-                self.client_cache.append(Client(id=id, name=firstname, middlename=secoundname))
+                all_phones=cells[5].text.splitlines()
+                self.client_cache.append(Client(id=id, name=firstname, middlename=secoundname,
+                                                home=all_phones[0], mobile=all_phones[1],
+                                                work=all_phones[2], phone2=all_phones[3]))
         return list(self.client_cache)
+
+    def open_client_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[6]
+        cell.find_element_by_tag_name("a").click()
+
+    def open_client_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements_by_name("entry")[index]
+        cell = row.find_elements_by_tag_name("td")[7]
+        cell.find_element_by_tag_name("a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_client_to_edit_by_index(index)
+        firstname=wd.find_element_by_name("firstname").get_attribute("value")
+        lastname=wd.find_element_by_name("lastname").get_attribute("value")
+        id=wd.find_element_by_name("id").get_attribute("value")
+        home=wd.find_element_by_name("home").get_attribute("value")
+        work=wd.find_element_by_name("work").get_attribute("value")
+        mobile=wd.find_element_by_name("mobile").get_attribute("value")
+        phone2=wd.find_element_by_name("phone2").get_attribute("value")
+        return Client(name=firstname, lastname=lastname, id=id, home=home,
+                      mobile=mobile, work=work, phone2=phone2)
+
+
--- test/test_add_client.py
+++ test/test_add_client.py
@@ -6,10 +6,10 @@ from model.client import Client
 def test_add_client(app):
     old_clients=app.client.get_client_list()
     client=Client(name="Daria", middlename="Yuryevna", lastname="Familia", nickname="daria",
-                            title="Title",address="Spb", home="Pskov", mobile="+79808887755", work="Company",
+                            title="Title",address="Spb", home="79999999999", mobile="79808887755", work="79999999999",
                             fax="+77000222", email="email@mail.ru", email2="email2@mail.ru", email3="email3@mail.ru",
                             bday="10",bmonth="August", byear="1999", aday="15", amonth="April", ayear="2020",
-                            address2="Spb2", phone2="+79808887756")
+                            address2="Spb2", phone2="79808887756")
     app.client.create_new_client(client)
     assert len(old_clients)+1 == app.client.count()
     new_clients=app.client.get_client_list()
@@ -19,10 +19,10 @@ def test_add_client(app):
 def test_add_client_new(app):
     old_clients=app.client.get_client_list()
     client = Client(name="Lena", middlename="Yuryevna", lastname="Familia", nickname="lena",
-                            title="Title",address="Spb", home="Pskov", mobile="+79808887755", work="Company",
+                            title="Title",address="Spb", home="79999999999", mobile="79808887755", work="79999999999",
                             fax="+77000222", email="email@mail.ru", email2="email2@mail.ru", email3="email3@mail.ru",
                             bday="10",bmonth="August", byear="1999", aday="15", amonth="April", ayear="2020",
-                            address2="Spb2", phone2="+79808887756")
+                            address2="Spb2", phone2="79808887756")
     app.client.create_new_client(client)
     assert len(old_clients)+1 == app.client.count()
     new_clients=app.client.get_client_list()
