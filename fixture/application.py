ef0ac2ebed8d57fdfa045defc12f8cb841652eec
dariabramenkova <dvchca_12@mail.ru>
2020-10-17 18:42:54 +0300

    Можно указывать тип бразузера и адрес строки

--- fixture/application.py
+++ fixture/application.py
@@ -8,17 +8,25 @@ from fixture.client import ClientHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser=="firefox":
+            self.wd = webdriver.Firefox()
+        elif browser=="chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.client = ClientHelper(self)
+        self.base_url=base_url
 
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath("//img[@alt='Edit']")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
 
     def is_element_present(self, how, what):
