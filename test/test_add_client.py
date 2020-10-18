699e0c5463758b524d61d111f41d5b069f94969d
dariabramenkova <dvchca_12@mail.ru>
2020-10-18 22:19:48 +0300

    Реализовать загрузку информации о тестируемом приложении из конфигурационного файла

--- test/test_add_client.py
+++ test/test_add_client.py
@@ -5,6 +5,7 @@ import pytest
 import string
 import random
 
+
 def random_string(prefix, maxlen):
     symbols=string.ascii_letters
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
