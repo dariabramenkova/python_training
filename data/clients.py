89e01a4bb0567e402a7094988e694c09a3dc5ebf
dariabramenkova <dvchca_12@mail.ru>
2020-10-19 15:49:43 +0300

    Реализовака загрузка тестовых данных из файлов

---
+++ data/clients.py
@@ -0,0 +1,15 @@
+from model.client import Client
+import string
+import random
+
+
+
+testdata=[
+    Client(name="name1", middlename="middlename1", lastname="lastname1", nickname="rrr", address="", home="8889999000",
+           mobile="8889999003", work="8889999004", email="as@mail.ru", email2="as2@mail.ru", email3="as3@mail.ru",
+           phone2="9889999002"),
+    Client(name="name2", middlename="middlename2", lastname="lastname2", nickname="ddd", address="", home="8889999001",
+           mobile="8889999002", work="8889999005", email="as4@mail.ru", email2="as5@mail.ru", email3="as@mail.ru6",
+           phone2="9889999004")
+]
+
--- test/test_add_client.py
+++ test/test_add_client.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.client import Client
-import pytest
-
-from data.add_client import constant as testdata
 
 
-@pytest.mark.parametrize("client", testdata, ids=[repr(x) for x in testdata])
-def test_add_client(app, client):
+def test_add_client(app, json_clients):
+    client=json_clients
     old_clients=app.client.get_client_list()
     app.client.create_new_client(client)
     assert len(old_clients)+1 == app.client.count()
