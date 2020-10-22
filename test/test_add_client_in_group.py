eae6846ce2e7ccf6309963193588fbb79875f822
dariabramenkova <dvchca_12@mail.ru>
2020-10-23 02:55:08 +0300

    Доработан тест на добавление контакта

--- test/test_add_client_in_group.py
+++ test/test_add_client_in_group.py
@@ -1,17 +1,22 @@
 from model.client import Client
 from model.group import Group
 from random import randrange
+import random
 from fixture.orm import ORMFixture
 
 orm=ORMFixture(host="localhost", name="addressbook", user="root", password="")
 
 def test_add_client_in_group(app, db):
+    clients=db.get_client_list()
+    groups=db.get_group_list()
+    group = random.choice(groups)
     if len(db.get_group_list())==0:
         app.group.create(Group(name="test"))
     if len(db.get_client_list())==0:
         app.client.create(Client(name="test"))
-    clients=db.get_client_list()
-    groups=db.get_group_list()
+    if len(orm.get_clients_not_in_group(group)) == 0:
+        app.client.create(Client(name="test"))
+
 
     while True:
         client_index=randrange(len(clients))
